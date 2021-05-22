983c9db2412f9cd2083998a9d4dca94819b03751
yallu201 <dlruddlf07@gmail.com>
2021-05-22 20:33:11 +0900

    Remove directory 'hooks'

--- src/hooks/useToggle.jsx
+++
@@ -1,8 +0,0 @@
-import { useState, useCallback } from "react";
-
-const useToggle = (initialValue = false) => {
-  const [isTrue, setIsTrue] = useState(initialValue);
-  const toggle = useCallback(() => setIsTrue((state) => !state), []);
-  return [isTrue, toggle];
-};
-export default useToggle;
